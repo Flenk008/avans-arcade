53892eab7ef93d534fb2c64a3fa5cb87ce0b8825
Flenk008 <frenk_0_0@hotmail.com>
2023-03-10 14:07:51 +0100

    111

--- src/ds.mk
+++ src/ds.mk
@@ -3,10 +3,13 @@ LD := gcc
 
 LFLAGS += -lSDL2
 
+<<<<<<< Updated upstream
 ifeq ($(HOST),Msys)
 SHARED_FLAGS += -I/mingw64/include
 endif
 
+=======
+>>>>>>> Stashed changes
 DESKTOP_SRCS += ppusim/sim.c \
 								ppusim/mem.c \
 								ppusim/pixel.c \
