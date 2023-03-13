8c00881961530b7d29beb0967b43a5e930377219
Flenk008 <frenk_0_0@hotmail.com>
2023-03-13 23:57:08 +0100

    entity collision bounce
    separate collisions for background and foreground sprites. Background has some bugs and collision methods are not working together properly. Also all code is in 1 file. Need to update that with correct naming

--- src/demo.c
+++ src/demo.c
@@ -14,19 +14,47 @@
 
 
 
+#define HH_DEMO_BALL_COUNT 2
+hh_s_ppu_loc_fam_entry g_hh_demo_balls[HH_DEMO_BALL_COUNT];
+
 hh_s_entity_player g_hh_player_1 = {
-	.pos_x		 = 31000, // 0b0000 0001 0011 0110
-	.pos_y		 = 21000,
-	.radius		 = 8,
+	.pos_x		 = 11000, 
+	.pos_y		 = 10000,
+	.radius		 = 4,
 	.speed		 = 100,
+	.air_speed   = 80,
 	.direction_x = 1,
+	.accelY 		 = 0,
+	.accelX 		 = 0,
 	.rotation	 = 8,
 	.in_air		 = false,
 };
 
-void hh_player_movement();
+hh_s_entity_player g_hh_enemy_1 = {
+	.pos_x		 = 18000, 
+	.pos_y		 = 18000,
+	.radius		 = 4,
+	.speed		 = 100,
+	.air_speed   = 80,
+	.direction_x = 1,
+	.accelY 		 = 0,
+	.accelX 		 = 0,
+	.rotation	 = 8,
+	.in_air		 = false,
+};
 
-uint16_t g_hh_pos_x; // 0b0000 0001 0011 0110
+void playerTest();
+hh_s_entity_player ent_coll(hh_s_entity_player tempPlayer, hh_s_entity_player tempEntity);
+bool distance_circles (hh_s_entity_player tempPlayer, hh_s_entity_player tempEntity);
+
+
+void hh_player_movement();
+hh_s_entity_player hh_enemy_movement(hh_s_entity_player tempEntity);
+
+int hh_background_collision_x(hh_s_entity_player tempEntity);
+hh_s_entity_player hh_background_collision_y_player(hh_s_entity_player tempEntity);
+
+uint16_t g_hh_pos_x = 1000; // 0b0000 0001 0011 0110
 uint16_t g_hh_pos_y;
 uint8_t g_hh_left  = 0;
 uint8_t g_hh_right = 0;
@@ -35,7 +63,7 @@ uint8_t g_hh_down  = 0;
 uint8_t g_hh_pos_x_bit[2];
 uint8_t g_hh_pos_y_bit[2];
 uint8_t g_hh_data_send[3];
-int g_hh_tile_x;
+int g_hh_tile_size = 8;
 int g_hh_tile_y;
 
 typedef struct {
@@ -50,14 +78,78 @@ void hh_demo_setup() {
 	hh_setup_palettes();
 	hh_setup_screen();
 
+	// load sprites
+	hh_ppu_update_sprite(0, HH_DBG_SPRITE_BALL);
+	hh_ppu_update_sprite(1, HH_DBG_SPRITE_CHECKERBOARD);
+
+	// background pattern
+	hh_ppu_update_color(0, 1, (hh_ppu_rgb_color_t){0x4, 0x4, 0x4});
+	for (unsigned i = 0; i < HH_PPU_BG_CANVAS_TILES_H * HH_PPU_BG_CANVAS_TILES_V; i++) {
+
+		hh_ppu_update_background(i, (hh_s_ppu_loc_bam_entry){
+			.horizontal_flip = false,
+			.vertical_flip	 = false,
+			.palette_index	 = 0,
+			.tilemap_index	 = 1,
+		});
+
+
+	}
+
+	// cool colors
+	hh_ppu_update_color(1, 1, (hh_ppu_rgb_color_t){0xf, 0x0, 0xf});
+	hh_ppu_update_color(2, 1, (hh_ppu_rgb_color_t){0xf, 0xf, 0xf});
+	hh_ppu_update_color(3, 1, (hh_ppu_rgb_color_t){0xf, 0x0, 0x0});
+	hh_ppu_update_color(4, 1, (hh_ppu_rgb_color_t){0x0, 0xf, 0xf});
+	hh_ppu_update_color(5, 1, (hh_ppu_rgb_color_t){0x0, 0x0, 0xf});
+
+	// balls
+	for (unsigned i = 0; i < HH_DEMO_BALL_COUNT; i++) {
+		g_hh_demo_balls[i].horizontal_flip = false;
+		g_hh_demo_balls[i].vertical_flip   = false;
+		g_hh_demo_balls[i].palette_index   = i + 1;
+		g_hh_demo_balls[i].tilemap_index   = 0;
+	}
+
 }
 
 void hh_demo_loop(unsigned long frame) {
 
-	// hh_player_movement();
+	 hh_player_movement();
 
-	hh_player_actions();
+	//playerTest();
+	
+	//hh_player_actions();
+// input testing (no hitbox stuff)
 
+
+	// g_hh_player_1.pos_x  += ((-1 * g_hh_controller_p1.dpad_left) + (1 * g_hh_controller_p1.dpad_right)) * g_hh_player_1.speed; // -1 = L || 1 == R
+	// g_hh_player_1.pos_y  += ((-1 * g_hh_controller_p1.dpad_up) + (1 * g_hh_controller_p1.dpad_down)) * g_hh_player_1.speed; // -1 = D || 1 == U
+
+	// ent_coll(g_hh_player_1,  g_hh_enemy_1);
+
+
+	// adjust map size
+	g_hh_pos_x = g_hh_player_1.pos_x / 100;
+	g_hh_pos_y = g_hh_player_1.pos_y / 100;
+
+	
+	// update player sprite on ppu
+	g_hh_demo_balls[0].position_x = g_hh_pos_x;
+	g_hh_demo_balls[0].position_y = g_hh_pos_y;
+
+	g_hh_demo_balls[1].position_x = g_hh_enemy_1.pos_x / 100;
+	g_hh_demo_balls[1].position_y = g_hh_enemy_1.pos_y / 100;;
+	hh_ppu_update_foreground(0, g_hh_demo_balls[0]);
+	hh_ppu_update_foreground(1, g_hh_demo_balls[1]);
+
+	// set background pattern position
+	hh_ppu_update_aux((hh_s_ppu_loc_aux){
+		.bg_shift_x = (frame / 2) % HH_PPU_SPRITE_WIDTH,
+		.bg_shift_y = (frame / 8) % HH_PPU_SPRITE_HEIGHT,
+		.fg_fetch	= 0,
+		.sysreset	= 0,
+	});
 }
 
 // void sendData(uint8_t address, uint16_t data) {
@@ -74,6 +166,12 @@ void hh_demo_loop(unsigned long frame) {
 void hh_player_movement() {
 	int8_t directionX = (-1 * g_hh_controller_p1.dpad_left) + (1 * g_hh_controller_p1.dpad_right); // -1 = L || 1 == R
 	int8_t directionY = (-1 * g_hh_controller_p1.dpad_up) + (1 * g_hh_controller_p1.dpad_down);	   // -1 = D || 1 == U
+
+	if(g_hh_controller_p1.button_primary == 1 && g_hh_player_1.accelY == 0 && g_hh_player_1.in_air == false)
+	{
+		g_hh_player_1.accelY = 25;
+	}
+
 
 	uint8_t i, j;
 	uint8_t rotation = 0; // 0-7
@@ -92,63 +190,274 @@ void hh_player_movement() {
 			rotation++;
 		}
 	}
-	// direction calc
-	if (directionX != 0) // update direction if player is not idle
-	{
-		g_hh_player_1.direction_x = directionX;
-	}
-	// collision map x-axis
+	
 
+
+	
+	
+	
+	g_hh_player_1 = ent_coll(g_hh_player_1,  g_hh_enemy_1);
+
+	if (g_hh_player_1.hit == true){
+		g_hh_demo_balls[0].palette_index   = 5;
+		g_hh_player_1.pos_x += g_hh_player_1.accelX * 100;
+		g_hh_player_1.pos_y -= g_hh_player_1.accelY * 200;
+		if(g_hh_player_1.accelX > 0){
+			g_hh_player_1.accelX--;
+		} else if (g_hh_player_1.accelX < 0){
+			g_hh_player_1.accelX++;
+		} else if (g_hh_player_1.accelX == 0){
+			g_hh_player_1.hit = false;
+		}
+		g_hh_player_1.accelY--;
+		
+	} else {
+		g_hh_demo_balls[0].palette_index   = 1;
+				// X-axis calc
+		if (directionX != 0) // update direction and position if player is not idle 
+		{
+			g_hh_player_1.direction_x = directionX;
+			g_hh_player_1.pos_x = hh_background_collision_x(g_hh_player_1);
+		}
+
+		g_hh_player_1 = hh_background_collision_y_player(g_hh_player_1);
+
+		// g_hh_player_1.pos_x  += ((-1 * g_hh_controller_p1.dpad_left) + (1 * g_hh_controller_p1.dpad_right)) * g_hh_player_1.speed; // -1 = L || 1 == R
+		 g_hh_player_1.pos_y  += ((-1 * g_hh_controller_p1.dpad_up) + (1 * g_hh_controller_p1.dpad_down)) * g_hh_player_1.speed; // -1 = D || 1 == U
+	}
+	
+	
+
+}
+
+
+int hh_background_collision_x(hh_s_entity_player tempEntity){
 	// tile calc including radius and direction for background coliision
 
 	uint16_t tileColX;
-	uint16_t tileColY = (g_hh_player_1.pos_y / 100) / 16;
+	uint16_t tileColY = (tempEntity.pos_y / 100) / 8;
 	;
 
 	// remaining space between grid and exact
 	uint8_t modTileX;
-	uint8_t modTileY;
 
-	if (g_hh_player_1.in_air == false && directionX != 0) {
-		if (directionX == 1) {
-			tileColX = ((g_hh_player_1.pos_x / 100) + g_hh_player_1.radius) / 16;
-			modTileX = (g_hh_player_1.pos_x + (100 * g_hh_player_1.radius)) % 1600;
-		} else if (directionX == -1) {
-			tileColX = ((g_hh_player_1.pos_x / 100) - g_hh_player_1.radius) / 16;
-			modTileX = (g_hh_player_1.pos_x - (100 * g_hh_player_1.radius)) % 1600;
-		}
 
-		if (HH_DEMO_HITBOX_TILEMAP[tileColY][tileColX + directionX] != 1) {
-			g_hh_player_1.pos_x = g_hh_player_1.pos_x + (directionX * g_hh_player_1.speed); // NEW x set
-		}
-
-		else if (HH_DEMO_HITBOX_TILEMAP[tileColY][tileColX + directionX] == 1) {
-			if (modTileX < g_hh_player_1.speed) {
-				g_hh_player_1.pos_x = g_hh_player_1.pos_x + (directionX * modTileX); // NEW x set
+		
+	if (tempEntity.direction_x == 1) {
+		tileColX = ((tempEntity.pos_x / 100) + tempEntity.radius) / g_hh_tile_size;
+		modTileX = 799 -((tempEntity.pos_x + (100 * tempEntity.radius)) % (g_hh_tile_size*100));
+	} else if (tempEntity.direction_x == -1) {
+		tileColX = ((tempEntity.pos_x / 100) - tempEntity.radius )/ g_hh_tile_size;
+		modTileX = (tempEntity.pos_x - (100 * tempEntity.radius)) % (g_hh_tile_size*100) ;
+	}
+	
+	tileColY = (( tempEntity.pos_y / 100) + tempEntity.radius) / g_hh_tile_size; //bottom of player box
+	
+	if (HH_DEMO_HITBOX_TILEMAP[tileColY][tileColX + tempEntity.direction_x] != 1) {
+		tileColY = (( tempEntity.pos_y / 100) - (tempEntity.radius)) / g_hh_tile_size; //bottom of player box
+		if (HH_DEMO_HITBOX_TILEMAP[tileColY][tileColX + tempEntity.direction_x] != 1) {
+			if(tempEntity.in_air== true){
+				tempEntity.pos_x= tempEntity.pos_x + (tempEntity.direction_x * tempEntity.air_speed); // 80% of max speed if in air
 			} else {
-				g_hh_player_1.pos_x = g_hh_player_1.pos_x + (directionX * g_hh_player_1.speed); // NEW x set
+				tempEntity.pos_x= tempEntity.pos_x + (tempEntity.direction_x * tempEntity.speed); // 80% of max speed if in air
+			}
+		} else {
+			if(tempEntity.in_air == true){
+				if (modTileX < tempEntity.air_speed) {
+					tempEntity.pos_x = tempEntity.pos_x + (tempEntity.direction_x * modTileX); // NEW x set
+				} else {
+					tempEntity.pos_x= tempEntity.pos_x + (tempEntity.direction_x * tempEntity.air_speed); //
+				}
+			} else {
+				if (modTileX < g_hh_player_1.speed) {
+					tempEntity.pos_x= tempEntity.pos_x + (tempEntity.direction_x * modTileX); // NEW x set
+				} else {
+					tempEntity.pos_x = tempEntity.pos_x + (tempEntity.direction_x * tempEntity.speed); // 
+				}
 			}
 		}
+	} else if (HH_DEMO_HITBOX_TILEMAP[tileColY][tileColX + tempEntity.direction_x] == 1) {
+		if(tempEntity.in_air == true){
+			if (modTileX < tempEntity.air_speed) {
+				tempEntity.pos_x = tempEntity.pos_x + (tempEntity.direction_x * modTileX); // NEW x set
+			} else {
+				tempEntity.pos_x = tempEntity.pos_x + (tempEntity.direction_x * tempEntity.air_speed); //
+			}
+		} else {
+			if (modTileX < g_hh_player_1.speed) {
+				tempEntity.pos_x = tempEntity.pos_x + (tempEntity.direction_x * modTileX); // NEW x set
+			} else {
+				tempEntity.pos_x= tempEntity.pos_x + (tempEntity.direction_x * tempEntity.speed); // 
+			}
+		}
+	}
 
-	} else // if in air different all borders have to be checked
-	{
+	
+	return tempEntity.pos_x;
+
+}
+
+hh_s_entity_player hh_background_collision_y_player(hh_s_entity_player tempEntity){
+
+	uint16_t tileColX;
+	uint16_t tileColY;
+	
+//
+	// remaining space between grid and exact
+		uint8_t modTileY;
+	
+	tileColY = ((tempEntity.pos_y / 100) + tempEntity.radius) / g_hh_tile_size; //bottom of player box
+	tileColX = ((tempEntity.pos_x / 100) + tempEntity.radius) / g_hh_tile_size; //right of player
+	modTileY = ((tempEntity.pos_y + (100 * tempEntity.radius)) % (g_hh_tile_size*100)); //bottom of box
+
+	if(tempEntity.accelY <= 0){ //falling
+		if(HH_DEMO_HITBOX_TILEMAP[tileColY+1][tileColX] != 1) {//check bottom right first
+			tileColX = ((tempEntity.pos_x / 100) - tempEntity.radius) / g_hh_tile_size;
+			if(HH_DEMO_HITBOX_TILEMAP[tileColY+1][tileColX] != 1)	{
+				tempEntity.in_air = true;
+				tempEntity.pos_y = tempEntity.pos_y - (tempEntity.accelY * 8) ;// NEW y set 
+				if (tempEntity.accelY > -35){
+					tempEntity.accelY--;
+				}
+			}
+			else	{
+				if(modTileY <= tempEntity.accelY * -8) {
+					tempEntity.pos_y = tempEntity.pos_y + (modTileY) ;// NEW y set 
+					tempEntity.accelY = 0;
+					tempEntity.in_air = false;
+				} else {
+					tempEntity.pos_y = tempEntity.pos_y - (tempEntity.accelY * 8) ;// NEW y set 
+				}				
+			}
+		} else {
+			if(modTileY <= tempEntity.accelY * -8) {
+				tempEntity.pos_y = tempEntity.pos_y + (modTileY) ;// NEW y set 
+				tempEntity.accelY = 0;
+				tempEntity.in_air = false;
+			} else {
+				tempEntity.pos_y = tempEntity.pos_y - (tempEntity.accelY * 8) ;// NEW y set 
+			}
+		}
+	} else { //jumping
+		tempEntity.in_air = true;
+		tileColY = (( tempEntity.pos_y / 100) - tempEntity.radius) / g_hh_tile_size; //top of player box
+		modTileY = 800 -((tempEntity.pos_y + (100 * tempEntity.radius)) % (g_hh_tile_size*100)); //top of box
+			if(HH_DEMO_HITBOX_TILEMAP[tileColY-1][tileColX] != 1) {//check top right first
+				tileColX = ((tempEntity.pos_x / 100) - tempEntity.radius) / g_hh_tile_size;
+				if(HH_DEMO_HITBOX_TILEMAP[tileColY-1][tileColX] != 1)	{
+					tempEntity.pos_y = tempEntity.pos_y - (tempEntity.accelY * 8) ;// NEW y set 
+					tempEntity.accelY--;
+				}	else	{
+				if(modTileY <= tempEntity.accelY * 8) {
+					tempEntity.pos_y = tempEntity.pos_y - (modTileY-1) ;// NEW y set 
+					tempEntity.accelY = 0;
+				} else {
+					tempEntity.pos_y = tempEntity.pos_y - (tempEntity.accelY * 8) ;
+					tempEntity.accelY--;
+				}
+				}
+			} else {
+				if(modTileY <= tempEntity.accelY * 8) {
+					tempEntity.pos_y = tempEntity.pos_y - (modTileY-1) ;// NEW y set 
+					tempEntity.accelY = 0;
+				} else {
+					tempEntity.pos_y = tempEntity.pos_y - (tempEntity.accelY * 8) ;// NEW y set 
+					tempEntity.accelY--;
+				}
+			}
+		
+	}
+	return tempEntity;
+}
+
+hh_s_entity_player hh_enemy_movement(hh_s_entity_player tempEntity){
+
+//g_hh_demo_balls[0].palette_index   = 3;
+return tempEntity;
+
+}
+
+void playerTest(){
+
+
+
+	g_hh_player_1 = ent_coll(g_hh_player_1,  g_hh_enemy_1);
+
+	if (g_hh_player_1.hit == true){
+		g_hh_player_1.pos_x += g_hh_player_1.accelX * 100;
+		g_hh_player_1.pos_y += g_hh_player_1.accelY * 100;
+		if(g_hh_player_1.accelX > 0){
+			g_hh_player_1.accelX--;
+		} else if (g_hh_player_1.accelX < 0){
+			g_hh_player_1.accelX++;
+		} else if (g_hh_player_1.accelX == 0){
+			g_hh_player_1.hit = false;
+		}
+		g_hh_player_1.accelY++;
+		
+	} else {
+		 g_hh_player_1.pos_x  += ((-1 * g_hh_controller_p1.dpad_left) + (1 * g_hh_controller_p1.dpad_right)) * g_hh_player_1.speed; // -1 = L || 1 == R
+		 g_hh_player_1.pos_y  += ((-1 * g_hh_controller_p1.dpad_up) + (1 * g_hh_controller_p1.dpad_down)) * g_hh_player_1.speed; // -1 = D || 1 == U
 	}
 
 
-	if(directionY != 0)
-	{
-	//	g_hh_player_1.pos_y = g_hh_player_1.pos_y + (directionY * g_hh_player_1.speed * 2); // NEW x set
+}
+
+
+
+hh_s_entity_player ent_coll(hh_s_entity_player tempPlayer, hh_s_entity_player tempEntity){
+	
+
+	bool collide;
+	if(tempPlayer.hit == false) {
+
+		collide = distance_circles( tempPlayer,  tempEntity);
 	}
-	// collision map floor (y-axis) (falling)
-	//  if falling no jump press (implement)
-	/*
-	tileColY = (( g_hh_player_1.pos_y / 100) + g_hh_player_1.radius) / 16; //bottom of player box
-	modTileY = 1;
-	if(HH_DEMO_HITBOX_TILEMAP[tileColY+1][tileColX] != 1) //rework after jumping
+	
+
+   
+	double angle = 0;
+	
+	if (collide == true)
 	{
-		g_hh_player_1.pos_y = g_hh_player_1.pos_y + 5 ;// NEW y set //makew var gravity
-		//playerStat = falling; //for later use of graphics/sound
+		//g_hh_demo_balls[0].palette_index   = 5;
+		//angle = atan2( tempEntity.pos_y - tempPlayer.pos_y, tempEntity.pos_x - tempPlayer.pos_x);
+		if(tempPlayer.pos_x <= tempEntity.pos_x) //player left of enemy
+		{
+			tempPlayer.accelY = 5;
+			tempPlayer.accelX = -8;
+		}	else {
+			tempPlayer.accelY = 5;
+			tempPlayer.accelX = 8;
+		}
+		tempPlayer.hit = true;
+		// lives--
+		// ghost mode / invulnerable
+
+	} else {
+		//g_hh_demo_balls[0].palette_index   = 1;
+		
 	}
-	*/
-	// else if(HH_DEMO_HITBOX_TILEMAP[])
+
+return tempPlayer;
+}
+
+
+bool distance_circles (hh_s_entity_player tempPlayer, hh_s_entity_player tempEntity){
+	int a_squared = (tempPlayer.pos_x - tempEntity.pos_x) * (tempPlayer.pos_x - tempEntity.pos_x);
+	int b_squared = (tempPlayer.pos_y - tempEntity.pos_y) * (tempPlayer.pos_y - tempEntity.pos_y);
+	int c_squared = a_squared + b_squared; 
+	int temp_player_r = tempPlayer.radius * 100;
+	int temp_entity_r =  tempEntity.radius * 100;
+	int radius = ( temp_entity_r + temp_player_r) * ( temp_entity_r + temp_player_r );
+
+		if( c_squared <= radius ){
+			return true;
+		} else {
+			return false;
+		}
+}
+
+void collision_bounce (){
+
 }
--- src/entity.h
+++ src/entity.h
@@ -8,7 +8,11 @@ typedef struct {
 	uint16_t pos_y;
 	uint8_t radius;
 	uint8_t rotation; //45 degrees steps 0 == right 2 == down 4 == left 6 == up
-	uint8_t direction_x; //direction where its looking at in case no input;
-	int8_t speed;	//10 default L/R MODifier
+	int8_t direction_x; //direction where its looking at in case no input;
+	int16_t speed;	//10 default L/R MODifier
+	int16_t air_speed;
+	int16_t accelY;
+	int16_t accelX;
 	bool in_air;
+	bool hit;
 } hh_s_entity_player;
